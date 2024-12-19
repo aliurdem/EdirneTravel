615af892cca98b1a9e46b79a95418997341434e7
Ali Eren Ürdem <107689594+aliurdem@users.noreply.github.com>
2024-12-19 12:48:33 +0300

    Update

--- EdirneTravel/Models/Dtos/TravelRouteDto.cs
+++ EdirneTravel/Models/Dtos/TravelRouteDto.cs
@@ -6,7 +6,7 @@ namespace EdirneTravel.Models.Dtos
     public class TravelRouteDto : BaseDto
     {
         public string Name { get; set; }
-        public string UserId { get; set; }
+        public string? UserId { get; set; }
         public string AverageDuration { get; set; }
         public byte[] ImageData { get; set; }
         public int CategoryId { get; set; }
--- EdirneTravel/Models/Dtos/TravelRoutePlaceDto.cs
+++ EdirneTravel/Models/Dtos/TravelRoutePlaceDto.cs
@@ -5,7 +5,7 @@ namespace EdirneTravel.Models.Dtos
     public class TravelRoutePlaceDto : BaseDto
     {
         public int Sequence { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
     }
 }
